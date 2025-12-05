2794598518c22014036c71c0d0971343f5d2cbcd
Ranvitha Chirumamilla <ranvithacoudexpert@gmail.com>
2025-12-04 20:43:31 -0500

    Updated React root mount to support global Wix floating widget

---
+++ public/chatbot-widget.js
@@ -0,0 +1,6 @@
+(function () {
+  const script = document.createElement("script");
+  script.src = "https://obk-chabot.netlify.app/assets/index.js";
+  script.defer = true;
+  document.body.appendChild(script);
+})();
