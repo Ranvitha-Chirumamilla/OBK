1ae44830fd404ade7654fc38be444c3c6301c71e
Ranvitha Chirumamilla <ranvithacoudexpert@gmail.com>
2025-12-09 22:15:53 -0500

    modified widget

--- public/chatbot-widget.js
+++ public/chatbot-widget.js
@@ -1,6 +1,6 @@
 (function () {
   const script = document.createElement("script");
-  script.src = "https://obk-chabot.netlify.app/assets/index-D0i7wbCL.js";
+  script.src = "https://obk-lime.vercel.app/assets/index.js";
   script.defer = true;
   document.body.appendChild(script);
 })();
